98181e6a5546685ada5f4e81d3f34373cad5a420
sepehr_sys <sepehrbizixer@gmail.com>
2026-02-18 14:35:40 +0330

    new methods

--- SS_random.py
+++ SS_random.py
@@ -7,10 +7,18 @@ class SS:
     def Random_integer(self, x, y):
         return self.r.randint(x, y)
     
+    def Choice_random_item(self, x):
+        return self.r.choice(x)
+    
+    def rptint(self, seq):
+        print(f'CPU : {seq}')
+    
 
 
 #test
 if __name__ == '__main__':
     r = SS()
     print(r.Random_integer(1, 10))
+    print(r.Choice_random_item(['ali', 'sepehr', 'masoomeh']))
+    r.rptint(r.Random_integer(1, 10))
         
