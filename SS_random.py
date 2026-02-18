e73735a4d9f414837e9e084b3318de42bd5efcaf
sepehr_sys <sepehrbizixer@gmail.com>
2026-02-18 14:26:59 +0330

    random number func created

--- SS_random.py
+++ SS_random.py
@@ -3,5 +3,14 @@ from random import *
 class SS:
     def __init__(self, s = None):
         self.r = Random(s)
-        
+
+    def Random_integer(self, x, y):
+        return self.r.randint(x, y)
+    
+
+
+#test
+if __name__ == '__main__':
+    r = SS()
+    print(r.Random_integer(1, 10))
         
